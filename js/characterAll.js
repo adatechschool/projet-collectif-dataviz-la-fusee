fe1c5f91e0ab9521bba691c486cf6608592f5a79
EliDevToBe <maxime.ktr@proton.me>
2024-03-19 16:26:33 +0100

    debut creation container et appel api character

--- js/characterAll.js
+++ js/characterAll.js
@@ -1,1 +1,29 @@
 // Welcome
+const charContainer = document.querySelector("#charContainer");
+
+async function displayCharacters() {
+    try {
+        const characterAll = await fetch("https://rickandmortyapi.com/api/character");
+        const characterAllJson = await characterAll.json();
+        const characterAllResults = await characterAllJson.results;
+
+        characterAllResults.forEach((data) => {
+
+            const newDiv = document.createElement("div");
+            const newImg = document.createElement("img");
+
+            newImg.setAttribute("src",)
+            newDiv.append
+            newDiv.append(data.name);
+            charContainer.append(newDiv)
+
+            console.log(data.name)
+        })
+
+        console.log(characterAllResults)
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+displayCharacters()
