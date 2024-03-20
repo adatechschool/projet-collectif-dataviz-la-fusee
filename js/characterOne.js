a7e00fcaa302765c4f81330f7c8b168a8e7969da
boris-valero <157314735+boris-valero@users.noreply.github.com>
2024-03-20 17:03:34 +0100

    intégration fetch

--- js/characterOne.js
+++ js/characterOne.js
@@ -1,3 +1,15 @@
+// const query = window.location.search;
+// console.log(query);
+
+// const params = new URLSearchParams(window.location.search);
+// const id = params.get("id");
+// console.log(id);
+
+// fetch(`https://rickandmortyapi.com/api/character/${id}`)
+//     .then (reponse => reponse.json())
+//     .then (reponse2 => {console.log(reponse2)
+//     })
+
 const query = window.location.search;
 console.log(query);
 
@@ -5,3 +17,7 @@ const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 console.log(id);
 
+fetch(`https://rickandmortyapi.com/api/character/${id}`)
+            .then(response => response.json()) // Transforme la réponse en JSON
+            .then(data => console.log(data))
+        
