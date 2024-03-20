e4300800d9dc72240ab9441be67e83be941894b6
Céline <celinesou77@gmail.com>
2024-03-20 16:45:40 +0100

    fetch et mise en page d'un seul épisode + afficher liste des noms des characters caméos

--- js/episodeOne.js
+++ js/episodeOne.js
@@ -1,6 +1,51 @@
-const query = window.location.search;
-console.log(query);
-
 const params = new URLSearchParams(window.location.search);
-const id = params.get("id");
-console.log(id);
+const episodeId = params.get("id");
+
+async function displayEpisode() {
+   try {
+      const episodeData = await fetch(
+         "https://rickandmortyapi.com/api/episode/" + episodeId
+      );
+      const episode = await episodeData.json();
+
+      //Episode title
+      let episodeTitle = document.querySelector(".episode_title");
+      episodeTitle.textContent = episode.name;
+
+      //Episode code
+      let episodeCode = document.querySelector(".episode_code");
+      const episodeSeason = episode.episode[1] + episode.episode[2];
+      const episodeNumber = episode.episode[4] + episode.episode[5];
+
+      episodeCode.textContent = `Season ${parseInt(
+         episodeSeason
+      )} - Episode ${parseInt(episodeNumber)}`;
+
+      //Episode aired date
+      let episodeDate = document.querySelector(".episode_aired");
+      episodeDate.textContent += episode.air_date;
+
+      //Episode list of characters
+      const idArray = episode.characters.map(
+         (charUrl) => charUrl.match(/\d+$/)[0]
+      );
+
+      console.log(`https://rickandmortyapi.com/api/character/${idArray}`);
+
+      const allCharactersData = await fetch(
+         `https://rickandmortyapi.com/api/character/${idArray}`
+      )
+      const allCharacters = await allCharactersData.json();
+
+      console.log(allCharacters);
+
+      allCharacters.forEach((character) => {
+         let li = document.createElement("li");
+         li.textContent = character.name;
+
+         document.querySelector(".character_list").append(li);
+      });
+   } catch {}
+}
+
+displayEpisode();
