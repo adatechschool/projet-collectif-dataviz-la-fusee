2fa3edfc88e5d9259496fbf101651fbaeee8b8da
EliDevToBe <maxime.ktr@proton.me>
2024-03-21 14:57:38 +0100

    branche à jour ?

--- js/characterAll.js
+++ js/characterAll.js
@@ -5,92 +5,85 @@ const btnPrevious = document.querySelector("#btnPrevious");
 const btnNext = document.querySelector("#btnNext");
 const subNavBar = document.querySelector("#subNavBar");
 const inputGoToPage = document.querySelector("#inputGoToPage");
-inputGoToPage.setAttribute("autocomplete", "off")
+inputGoToPage.setAttribute("autocomplete", "off");
 
 async function displayCharacters() {
-    try {
+  try {
+    const params = new URLSearchParams(window.location.search);
+    let idPage = params.get("page");
+    // ==== Limitation de la page max
+    if (parseInt(idPage) > 42) idPage = "42";
 
-        const params = new URLSearchParams(window.location.search)
-        let idPage = params.get("page")
-        // ==== Limitation de la page max
-        if (parseInt(idPage) > 42) idPage = "42"
+    let characterAll = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${idPage}`
+    );
+    const characterAllJson = await characterAll.json();
+    const characterAllResults = await characterAllJson.results;
+    const characterAllInfo = await characterAllJson.info;
 
-        let characterAll = await fetch(`https://rickandmortyapi.com/api/character?page=${idPage}`);
-        const characterAllJson = await characterAll.json();
-        const characterAllResults = await characterAllJson.results;
-        const characterAllInfo = await characterAllJson.info;
+    // ==== URLs des pages Next et Prev
+    const URLPageNext = characterAllInfo.next;
+    const URLPagePrev = characterAllInfo.prev;
+    // == Fin URLs
 
-        // ==== URLs des pages Next et Prev
-        const URLPageNext = characterAllInfo.next
-        const URLPagePrev = characterAllInfo.prev
-        // == Fin URLs
+    // ==== Creation dynamique de la page
+    characterAllResults.forEach((data) => {
+      const newDiv = document.createElement("div");
+      const newDiv2 = document.createElement("div");
+      const newImg = document.createElement("img");
 
-        // ==== Creation dynamique de la page
-        characterAllResults.forEach((data) => {
+      newImg.setAttribute(
+        "src",
+        `https://rickandmortyapi.com/api/character/avatar/${data.id}.jpeg`
+      );
 
-            const newDiv = document.createElement("div");
-            const newDiv2 = document.createElement("div");
-            const newImg = document.createElement("img");
+      newDiv.append(newImg);
+      charContainer.append(newDiv);
+      newDiv2.append(data.name);
+      charContainer.append(newDiv2);
 
-            newImg.setAttribute("src", `https://rickandmortyapi.com/api/character/avatar/${data.id}.jpeg`);
+      newImg.addEventListener("click", () => {
+        location.href = `./characterOne.html?id=${data.id}`;
+      });
+    });
+    // == Fin page dynamique
 
-            newDiv.append(newImg);
-            charContainer.append(newDiv);
-            newDiv2.append(data.name);
-            charContainer.append(newDiv2);
+    // ==== Gestion des pages Next/Prev et Go-To-Page
+    {
+      // = Next
+      btnNext.addEventListener("click", (e) => {
+        e.preventDefault();
 
-            newImg.addEventListener("click", () => {
-                location.href = `./characterOne.html?id=${data.id}`
-            })
+        if (URLPageNext) {
+          let params = new URL(URLPageNext).searchParams;
+          idPage = params.get("page");
+        } else idPage = "1";
 
-        })
-        // == Fin page dynamique
+        location.href = `./characterAll.html?page=${idPage}`;
+      });
 
-        // ==== Gestion des pages Next/Prev et Go-To-Page
-        {
-            // = Next
-            btnNext.addEventListener("click", (e) => {
-                e.preventDefault();
+      // = Prev
+      btnPrevious.addEventListener("click", (e) => {
+        e.preventDefault();
 
-                if (URLPageNext) {
-                    let params = new URL(URLPageNext).searchParams;
-                    idPage = params.get("page");
-                } else idPage = "1"
+        if (URLPagePrev) {
+          let params = new URL(URLPagePrev).searchParams;
+          idPage = params.get("page");
+        } else idPage = "42";
 
-                location.href = `./characterAll.html?page=${idPage}`
-            })
+        location.href = `./characterAll.html?page=${idPage}`;
+      });
 
-            // = Prev
-            btnPrevious.addEventListener("click", (e) => {
-                e.preventDefault();
+      inputGoToPage.value = idPage;
+      inputGoToPage.addEventListener("change", (e) => {
+        e.preventDefault();
 
-                if (URLPagePrev) {
-                    let params = new URL(URLPagePrev).searchParams;
-                    idPage = params.get("page");
-                } else idPage = "42"
-
-                location.href = `./characterAll.html?page=${idPage}`
-            })
-
-            inputGoToPage.value = idPage
-            inputGoToPage.addEventListener("change", (e) => {
-                e.preventDefault();
-
-                idPage = inputGoToPage.value
-                location.href = `./characterAll.html?page=${idPage}`
-
-            })
-
-        }// == Fin gestion pages Next/Prev et Go-To-Page
-
-
-
-    } catch (error) {
-        console.error(error)
-    }
+        idPage = inputGoToPage.value;
+        location.href = `./characterAll.html?page=${idPage}`;
+      });
+    } // == Fin gestion pages Next/Prev et Go-To-Page
+  } catch (error) {
+    console.error(error);
+  }
 }
-displayCharacters()
-
-
-
-
+displayCharacters();
