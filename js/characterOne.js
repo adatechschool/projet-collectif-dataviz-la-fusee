9453ed3a96e53a54b73794129abb530a2d37f603
boris-valero <157314735+boris-valero@users.noreply.github.com>
2024-03-26 12:02:16 +0100

    MAJ JS
    MAJ JS

--- js/characterOne.js
+++ js/characterOne.js
@@ -20,21 +20,26 @@ console.log(id);
 fetch(`https://rickandmortyapi.com/api/character/${id}`)
             .then(response => response.json()) // Transforme la réponse en JSON
             .then(data => {
+              
               const image = document.createElement("img");
               image.src = data.image;
               document.getElementById("card-photo").append(image);
+              
               let characterName = document.querySelector(".character_name");
               characterName.textContent = data.name;
+              
               let characterStatus = document.querySelector(".character_status");
               characterStatus.textContent = data.status;
+              
               let characterSpecies = document.querySelector(".character_species");
               characterSpecies.textContent = data.species;
+              
               let characterGender = document.querySelector(".character_gender");
               characterGender.textContent = data.gender;
+              
               let characterOriginName = document.querySelector(".character_origin_name");
               characterOriginName.textContent = data.origin.name;
-              let characterLocationName = document.querySelector(".character_location_name");
-              characterLocationName.textContent = data.location.name;
-              let characterSign = document.querySelector(".character_sign");
+              
+            let characterSign = document.querySelector(".character_sign");
               characterSign.textContent = data.name;
             })
