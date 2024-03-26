983674e7c60d31ce27f0de435d82c3257108eb1c
CarlosPTN <carlosmioli1@gmail.com>
2024-03-26 17:17:11 +0100

    added my changes from episodeAll including the style

--- js/episodeAll.js
+++ js/episodeAll.js
@@ -8,7 +8,6 @@ const getSeason = function (episode) {
   return seasonNum;
 };
 // getSeason("S01E05");
-const divideEpiSeason = function (data) { };
 
 // document.querySelector(".nav__links").addEventListener("click", function (e) {
 //   e.preventDefault();
@@ -26,9 +25,8 @@ const allEpisodes = async function () {
   try {
     let allEpisodesArray = [];
     let episodesPerSeason = [];
-    let htmlTableSeasons = "";
-    let htmlTableEpisodes = "";
-    let currentSeason = 0;
+    let htmlSeasonLinks = "";
+    let htmlSeasonContents = "";
 
     const url = `https://rickandmortyapi.com/api/episode`;
     const allEpisodesFetch = await fetch(url);
@@ -36,62 +34,68 @@ const allEpisodes = async function () {
     // console.log(allEpisodesData.info.pages);
     // console.log(allEpisodesData);
     for (let i = allEpisodesData.info.pages; i > 0; i--) {
-      const url = `https://rickandmortyapi.com/api/episode?page=${i}`;
-      const allEpisodesFetch = await fetch(url);
-      const allEpisodesData = await allEpisodesFetch.json();
-      allEpisodesArray.unshift(allEpisodesData.results);
+      const pageUrl = `https://rickandmortyapi.com/api/episode?page=${i}`;
+      const pageFetch = await fetch(pageUrl);
+      const pageData = await pageFetch.json();
+      allEpisodesArray.unshift(...pageData.results);
       //   console.log(allEpisodesData);
     }
 
-    allEpisodesArray = allEpisodesArray.flat();
+    // allEpisodesArray = allEpisodesArray.flat();
     allEpisodesArray.forEach((episode) => {
-      const season = getSeason(episode.episode);
-      if (episodesPerSeason[season - 1]) {
-        episodesPerSeason[season - 1].push(episode);
-      } else {
-        episodesPerSeason[season - 1] = [episode];
-      }
+      const seasonNumber = getSeason(episode.episode);
+      if (!episodesPerSeason[seasonNumber - 1])
+        episodesPerSeason[seasonNumber - 1] = [];
+      episodesPerSeason[seasonNumber - 1].push(episode);
     });
 
     console.log(allEpisodesArray);
     console.log(episodesPerSeason);
-    divideEpiSeason(allEpisodesData);
-    episodesPerSeason.forEach((_, i) => {
-      htmlTableSeasons += `               
-      <a class="nav__item nav__link" href="#section--${i + 1}">${i + 1}</a>
-        `;
-    });
-    allEpisodesArray.forEach((episode, i) => {
-      if (getSeason(episode.episode) === currentSeason) {
-        htmlTableEpisodes += `
-    <tr>
-        <td># ${i + 1}</td>
-        <td>${episode.name}</td>
-    </tr>
-    
-        `;
-      } else {
-        htmlTableEpisodes += `
-        <section class="section" id="section--${getSeason(episode.episode)}">
-        <tr>
-            <td>Season ${getSeason(episode.episode)}</td>
-        </tr>
-        <tr>
-            <td># ${i + 1}</td>
-            <td>${episode.name}</td>
-        </tr>
-            `;
-        currentSeason = getSeason(episode.episode);
-      }
+    // divideEpiSeason(allEpisodesData);
+
+    episodesPerSeason.forEach((episodes, i) => {
+      if (!episodes) return;
+
+      htmlSeasonLinks += `<a class="nav__item nav__link" href="#season-${
+        i + 1
+      }">Season ${i + 1}</a>`;
+
+      let seasonEpisodesHtml = episodes
+        .map(
+          (episode) =>
+            `<div class="episodeUniAll">
+          <div class="episode-number">#${episode.episode}</div>
+          <div class="episode-name">${episode.name}</div>
+         </div>`
+        )
+        .join("");
+
+      htmlSeasonContents += `
+        <div class="season-container" id="season-${i + 1}" data-season="${
+        i + 1
+      }" style="background-image: url('images/season${i}.png');  ">
+          <h2 class="season">Season ${i + 1}</h2>
+          <div class="episodes-container">
+            ${seasonEpisodesHtml}
+          </div>
+        </div>
+      `;
     });
 
-    navSeasonContainer.insertAdjacentHTML("beforeend", htmlTableSeasons);
-    navEpisodeContainer.insertAdjacentHTML("beforeend", htmlTableEpisodes);
-
-    let apparitionRate = {}
-    allEpisodesArray.forEach((episode) => {
-      let charInEpisode = episode.characters
-    })
+    // episodes.addEventListener("click", (e) => {
+    //   console.log(e);
+    // });
+    navSeasonContainer.innerHTML = htmlSeasonLinks;
+    navEpisodeContainer.innerHTML = htmlSeasonContents;
+    const episode = document.querySelectorAll(".episode");
+    console.log(episode);
+    // console.log(episode);
+    episode.forEach((ep, i) => {
+      ep.addEventListener("click", () => {
+        location.href = `./episodeOne.html?id=${i + 1}`;
+        console.log(this);
+      });
+    });
   } catch (error) {
     console.error("Found an error", error);
   }
